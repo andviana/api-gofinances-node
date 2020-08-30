01ce6fdebd6ae2d6d179ae9d1bba5454e09de416
Anderson Viana <andiana@hotmnai.com>
2020-08-30 08:34:20 -0300

    balance calcutation implemented

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,8 +11,13 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const income = transactions.reduce((acc:number, curr:Transaction)=>(curr.type==='income')? acc + curr.value: acc,0);
+    const outcome = transactions.reduce((acc:number, curr:Transaction)=>(curr.type==='outcome')? acc + curr.value: acc,0);
+    const total = income - outcome;
+    return {income, outcome, total};
   }
 }
 
 export default TransactionsRepository;
+
