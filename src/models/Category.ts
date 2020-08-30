5421ef11a39979412632b19124c0d8015eb07cb1
Anderson Viana <andiana@hotmnai.com>
2020-08-30 07:40:14 -0300

    category and transaction models has been  mapped with typeorm

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,17 @@
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn } from 'typeorm'
+@Entity('categories')
 class Category {
+
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
